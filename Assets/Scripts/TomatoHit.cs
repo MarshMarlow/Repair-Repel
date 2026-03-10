3a8013eaea64ef7dde37c8d36e2c133311466d13
kenneth-li04 <kenneth1105@live.ca>
2026-03-10 00:55:31 -0400

    added tomato ink screen covers

--- Assets/Scripts/TomatoHit.cs
+++ Assets/Scripts/TomatoHit.cs
@@ -4,18 +4,62 @@ using System.Collections;
 
 public class TomatoHit : MonoBehaviour
 {
-    public GameObject damagePanel;
-    public float flashTime = 0.2f; 
+    //public GameObject damagePanel;
+    public GameObject inkPrefab;
+    public Transform canvas;
+    public Sprite[] ink;
+    public float flashTime = 0.5f; 
+    public float fadeTime = 2f;
 
     public void FlashRed()
     {
-        StartCoroutine(FlashCoroutine());
+        SpawnInk();
     }
 
-    private IEnumerator FlashCoroutine()
+    void SpawnInk()
     {
-        damagePanel.SetActive(true);
+        GameObject splat = Instantiate(inkPrefab, canvas);
+        Image img = splat.GetComponent<Image>();
+
+        img.sprite = ink[Random.Range(0, ink.Length)];
+
+        RectTransform rect = splat.GetComponent<RectTransform>();
+
+        rect.anchorMin = Vector2.zero;
+        rect.anchorMax = Vector2.one;
+        rect.offsetMin = Vector2.zero;
+        rect.offsetMax = Vector2.zero;
+
+        StartCoroutine(FlashCoroutine(img));
+    }
+
+
+    private IEnumerator FlashCoroutine(Image image)
+    {
+
+        image.sprite = ink[Random.Range(0, ink.Length)];
+        
+        Color c = image.color;
+        c.a = 1f;
+        image.color = c;
+
         yield return new WaitForSeconds(flashTime);
-        damagePanel.SetActive(false);
+
+        float t = 0;
+
+        while (t < fadeTime)
+        {
+            t += Time.deltaTime;
+
+            c.a = Mathf.Lerp(1f, 0f, t / fadeTime);
+            image.color = c;
+
+            yield return null;
+        }
+        
+        c.a = 0f;
+        image.color = c;
+
+        Destroy(image.gameObject);
     }
 }
